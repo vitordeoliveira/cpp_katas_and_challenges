d24df54a446a8c332d1b2059c38968aa478b4ecc
Vitor de Oliveira <vitordeoliveira50@gmail.com>
2023-01-16 16:43:48 +0100

    .

--- leetcode/Data Structure I/Maximum_Subarray.cpp
+++ leetcode/Data Structure I/Maximum_Subarray.cpp
@@ -22,16 +22,38 @@
 
 // Kadane's Algorithm
 
-#include "vector"
+#include <vector>
 #include "iostream"
+#include "limits.h"
+ #include <numeric>
 
 class Solution {
 public:
     int maxSubArray(std::vector<int>& nums) {
+        int mid = (nums.size()-1) / 2;
+        int max_sum_r = INT_MIN;
+        int max_sum_l = INT_MIN;
+        int max_sum = INT_MIN;
+
+        if( nums.size()==1 ) return nums[0];
+
+        std::vector<int> s(6,0);
+
+        // max_sum_r = maxSubArray(s);
+        // max_sum_l = maxSubArray(std::vector<int>(nums.begin(), nums.begin()+mid));
+
+
         return 0;
     }
 };
 
 int main(){
+
+    Solution s;
+
+    std::vector<int> v = {1, 2, 6, 4};
+
+    s.maxSubArray(v);
+
     return 0;
 }
