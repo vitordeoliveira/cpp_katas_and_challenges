dfe6994e1b59bb79f67a5f97a5efcbce6c60b53b
Vitor de Oliveira <vitordeoliveira50@gmail.com>
2023-02-04 18:37:01 +0100

    finish solution for search matrix in 2d

--- leetcode/Data Structure I/Search_a_2D_Matrix.cpp
+++ leetcode/Data Structure I/Search_a_2D_Matrix.cpp
@@ -56,39 +56,122 @@ public:
     //     return -1;
     // }
 
-        bool searchMatrix(const vector<vector<int>> &matrix, int target)
-    {
-        int x = binarySearch(matrix, 0, 8, target);
-        if (x != -1)
-            return true;
-        return false;
-    }
+    // bool searchMatrix(const vector<vector<int>> &matrix, int target)
+    // {
+    //     int mid = matrix.size() / 2;
 
-    int binarySearch(const vector<vector<int>> vec, int left, int right, int x)
-    {
+    //     while (mid < matrix.size() && mid > -1)
+    //     {
+    //         pair<int, int> x = binarySearch(matrix[mid], 0, matrix[mid].size(), target);
+    //         if (x.first != -1)
+    //             return true;
 
-        if (right >= left)
+    //         if (x.second == 0)
+    //         {
+    //             mid--;
+    //         }
+    //         else if (x.second > matrix[mid].size())
+    //         {
+    //             mid++;
+    //         }
+    //     }
+
+    //     return false;
+    // }
+
+    // pair<int, int> binarySearch(const vector<int> vec, int left, int right, int x)
+    // {
+    //     int mid = left + (right - left) / 2;
+
+    //     if (right >= left)
+    //     {
+    //         int value = vec[mid];
+
+    //         if (vec[mid] == x)
+    //             return {mid, 0};
+    //         if (vec[mid] > x)
+    //             return binarySearch(vec, left, mid - 1, x);
+    //         return binarySearch(vec, mid + 1, right, x);
+    //     }
+
+    //     return {-1, mid};
+    // }
+
+    vector<int> reshape(const vector<vector<int>> &matrix)
+    {
+        int elements = 0;
+        for (const auto &row : matrix)
         {
-            int mid = left + (right - left) / 2;
-
-            int value = vec[0][mid];
-
-            if (vec[0][mid] == x)
-                return mid;
-            if (vec[0][mid] > x)
-                return binarySearch(vec, left, mid - 1, x);
-            return binarySearch(vec, mid + 1, right, x);
+            elements += row.size();
         }
 
-        return -1;
+        vector<int> vec(elements);
+        int k = 0;
+        for (const auto &row : matrix)
+        {
+            for (int x : row)
+            {
+                vec[k++] = x;
+            }
+        }
+        return vec;
     }
+
+    bool searchMatrix(const vector<vector<int>> &matrix, int target)
+    {
+        vector<int> vec = reshape(matrix);
+        return binarySearch(vec, 0, vec.size(), target);
+    }
+
+    int binarySearch(const vector<int> &vec, int left, int right, int x)
+    {
+        if (left >= right)
+        {
+            return false;
+        }
+        int mid = left + (right - left) / 2;
+        if (vec[mid] == x)
+        {
+            return true;
+        }
+        else if (vec[mid] > x)
+        {
+            return binarySearch(vec, left, mid, x);
+        }
+        else
+        {
+            return binarySearch(vec, mid + 1, right, x);
+        }
+    }
+
+    // bool searchMatrix(const vector<vector<int>> &matrix, int target)
+    // {
+    //     int rows = matrix.size();
+    //     if (rows == 0)
+    //         return false;
+    //     int cols = matrix[0].size();
+    //     if (cols == 0)
+    //         return false;
+
+    //     int row = 0, col = cols - 1;
+    //     while (row < rows && col >= 0)
+    //     {
+    //         if (matrix[row][col] == target)
+    //             return true;
+    //         if (matrix[row][col] < target)
+    //             row++;
+    //         else
+    //             col--;
+    //     }
+    //     return false;
+    // }
 };
 
 int main()
 {
     Solution s;
 
-    s.searchMatrix({{1, 3, 5}, {10, 11, 13}, {23, 30, 3460}}, 13);
+    s.searchMatrix({{1, 3, 5}, {10, 12, 13}, {14, 30, 3460}}, 1);
 
     // cout << s.binarySearch({1,3,4,5}, 0, 3, 0) << '\n';
 }
