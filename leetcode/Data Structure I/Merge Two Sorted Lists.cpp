0109d2ac84df87ea4d921069339181c6a63a70e7
Vitor de Oliveira <vitordeoliveira50@gmail.com>
2023-02-13 18:02:12 +0100

    little fix

--- leetcode/Data Structure I/Merge Two Sorted Lists.cpp
+++ leetcode/Data Structure I/Merge Two Sorted Lists.cpp
@@ -79,10 +79,6 @@ public:
     {
         ListNode *head = new ListNode();
         ListNode *curr = head;
-        if (!list1)
-            return list2;
-        if (!list2)
-            return list1;
 
         while (list1 != NULL && list2 != NULL)
         {
@@ -99,16 +95,10 @@ public:
             curr = curr->next;
         }
 
-// while(list1 != NULL && list2 != NULL){
-//              if(list1->val <= list2->val){
-//                  curr->next=list1;
-//                  list1 = list1 ->next;
-//              }else{
-//                  curr->next=list2;
-//                  list2 = list2 ->next;
-//              }
-//               curr = curr -> next;
-//          }
+        if (list1 != NULL)
+            curr->next = list1;
+        if (list2 != NULL)
+            curr->next = list2;
         return head->next;
     };
 };
