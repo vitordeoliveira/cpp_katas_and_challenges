fec933bfa3a22d7508a20542d0658613aeb3a845
Vitor de Oliveira <vitordeoliveira50@gmail.com>
2023-03-11 08:53:19 +0100

    Symmetric Tree init

---
+++ leetcode/Data Structure I/Symmetric Tree.cpp
@@ -0,0 +1,19 @@
+// 101. Symmetric Tree
+// Easy
+// 12.4K
+// 280
+// Companies
+// Given the root of a binary tree, check whether it is a mirror of itself (i.e., symmetric around its center).
+
+ 
+
+// Example 1:
+
+
+// Input: root = [1,2,2,3,4,4,3]
+// Output: true
+// Example 2:
+
+
+// Input: root = [1,2,2,null,3,null,3]
+// Output: false
